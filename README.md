76546497933202084ecd9b5b0f351b74ac592972
KASNY <75784852+KASNY@users.noreply.github.com>
2023-11-26 15:18:42 +0100

    v1.0
    Added app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # business-card
 Creating business card
+
+Open the program, enter your name, surname and phone number, and then the card.txt file will display these data
---
+++ Saving information/Saving information.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::string name, surname;
+
+    int phone_number;
+
+    std::cout << "Enter name: ";
+    std::cin >> name;
+
+    std::cout << "Enter surname: ";
+    std::cin >> surname;
+
+    std::cout << "Enter phone number: ";
+    std::cin >> phone_number;
+
+    std::fstream file;
+    file.open("card.txt", std::ios::out | std::ios::app);
+
+    file << name << std::endl;
+    file << surname << std::endl;
+    file << phone_number << std::endl;
+
+    file.close();
+
+}
